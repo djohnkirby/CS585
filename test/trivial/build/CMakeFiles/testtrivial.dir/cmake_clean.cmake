c3ef6ce2c740c27f8eaa5b2a90625c5b312ad859
Daniel John Kirby <dkirby@cmu.edu>
2015-09-06 10:04:24 -0400

    IT WORKS! IT WORKS! I FINALLY INVENTED SOMETHING THAT WORKS!

---
+++ test/trivial/build/CMakeFiles/testtrivial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/testtrivial.dir/trivial.t.cpp.o"
+  "testtrivial.pdb"
+  "testtrivial"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/testtrivial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
