f86d71e9ee741aadc4179b52e35595f16557ede1
Daniel John Kirby <dkirby@cmu.edu>
2015-09-07 12:52:00 -0400

    removed build from repo

--- build/gmock/src/gmock-stamp/verify-gmock.cmake
+++
@@ -1,4 +0,0 @@
-set(file "/home/dkirby/CS585/build/gmock/src/gmock-1.7.0.zip")
-message(STATUS "verifying file...
-     file='${file}'")
-message(STATUS "verifying file... warning: did not verify file - no URL_HASH specified?")
--- build/gtest/src/gtest-stamp/verify-gtest.cmake
+++
@@ -1,4 +0,0 @@
-set(file "/home/dkirby/CS585/build/gtest/src/gtest-1.7.0.zip")
-message(STATUS "verifying file...
-     file='${file}'")
-message(STATUS "verifying file... warning: did not verify file - no URL_HASH specified?")
